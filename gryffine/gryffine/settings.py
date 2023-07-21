03ecce3c862d882ce7f0ffe38ffa75f76e050904
PressXToWin <PressXToWin@users.noreply.github.com>
2023-07-21 14:06:38 +0700

    bool env variables fix

--- gryffine/gryffine/settings.py
+++ gryffine/gryffine/settings.py
@@ -5,26 +5,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'key')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', False)
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split()
+
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split()
 
 AUTH_USER_MODEL = 'users.User'
 LOGIN_URL = 'users:login'
 LOGIN_REDIRECT_URL = 'records:index'
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -73,20 +66,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'gryffine.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -103,10 +88,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/3.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -117,15 +98,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
@@ -139,6 +113,6 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 CONSIDER_LOCAL_WHITELISTED = True
 
-NOTIFY_NOT_SUCCESSFUL = True
-NOTIFY_NOT_SUSPICIOUS = True
-NOTIFY_UNKNOWN_SUSPICIOUS = True
+NOTIFY_NOT_SUCCESSFUL = os.getenv("NOTIFY_NOT_SUCCESSFUL", "True") == "True"
+NOTIFY_NOT_SUSPICIOUS = os.getenv("NOTIFY_NOT_SUSPICIOUS", "True") == "True"
+NOTIFY_UNKNOWN_SUSPICIOUS = os.getenv("NOTIFY_UNKNOWN_SUSPICIOUS", "True") == "True"
